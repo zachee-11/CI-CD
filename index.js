fce70217fc131dc96d6bb7bd8fa4759629018c89
zachee-11 <woumbez@gmail.com>
2025-08-03 22:43:14 +0200

    feat: ajout ci.

--- index.js
+++ index.js
@@ -2,4 +2,8 @@ function add(a, b) {
   return a + b;
 }
 
-module.exports = { add };
+function sub(a,b) {
+    return a-b ;
+}
+
+module.exports = { add, sub };
--- tests/index.test.js
+++ tests/index.test.js
@@ -1,5 +1,10 @@
-const { add } = require('../index');
+const { default: test } = require('node:test');
+const { add, sub } = require('../index');
 
 test('additionne 2 + 3 pour donner 5', () => {
   expect(add(2, 3)).toBe(5);
 });
+
+test('soustraction 3-2 pour donner 1', () => {
+    expect(sub(3,2)).toBe(1);
+})
